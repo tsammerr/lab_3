1c6fbf57c685f7e14fc9c1c9764a3d2855e7246d
Maria <tarasovych.maria@gmail.com>
2022-10-18 22:18:46 +0300

    init: ex 1

--- main.py
+++
@@ -1,8 +0,0 @@
-try:
-    start = int(input('start -> '))
-    end = int(input('end -> '))
-    for item in range(start, end):
-        print(f'sum = {start} + {end} = {start + end}\navg = {}')
-        start+=1
-except Exception as ex:
-    print(ex)
